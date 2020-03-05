42f68f5a862b6c99484d9d8f3ee9121a49a83659
jw59 <junw59@outlook.com>
2020-03-05 10:24:24 +0800

    this new soft study

---
+++ he.md
@@ -0,0 +1,14 @@
+# hello world  
+
+this is new editor
+
+and maybe its good  
+zheshix
+
+>shi
+>>sjoaj of
+---
+
+```python
+print('hello world')
+```
---
+++ newworl.py
@@ -0,0 +1,11 @@
+print('hello world!')
+import numpy as np
+def fuc(nons):
+    print('this is a new editor!')
+    return 0
+
+fuc(0)
+import time
+t=time.ctime()
+print(t)
+109
